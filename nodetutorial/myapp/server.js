0ca04933d159c49351ba65570a4c2cf7ae7f43ff
Vaibhav Agarwal <agarwal.vaibhav046@gmail.com>
2020-12-01 17:07:28 +0530

    Added Images + Changes to server.js

--- nodetutorial/myapp/server.js
+++ nodetutorial/myapp/server.js
@@ -3,18 +3,29 @@ const app = express();
 const passport = require('passport');
 const { JWTStrategy } = require('@sap/xssec');
 const xsenv = require('@sap/xsenv');
+const bodyParser = require('body-parser');
+app.use(bodyParser.json({ extended: true }));
 
 // XSUAA Middleware
 passport.use(new JWTStrategy(xsenv.getServices({uaa:{tag:'xsuaa'}}).uaa));
 
 app.use(passport.initialize());
-app.use(passport.authenticate('JWT', { session: false }));
+ app.use(passport.authenticate('JWT', { session: false }));
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.post("/")
+app.post('/GetData', function (req, res) {
+  console.log("Request data:");
+  console.log(req.body);
+  res.json({
+    "Location": "Melo",
+    "ID": "1234",
+    "Status": "Ack"
+  });
+});
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
